28643a1ccd79c69e52a4d9090e271f383f32f867
marc.luettecke <marc.luettecke@uni-konstanz.de>
2020-06-03 21:30:38 +0200

    servers listen correction

--- app.js
+++ app.js
@@ -63,8 +63,8 @@ app.use("/memories", memoriesRoutes)
 app.use("/memories/:id/comments", commentRoutes)
 
 //start server
-var server_port = process.env.YOUR_PORT || process.env.PORT || port;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+const server_port = process.env.YOUR_PORT || process.env.PORT || port;
+const server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
